45ce3791e580a71eb4223681aa979814ce51c605
poyuaki <csubaru07@gmail.com>
2022-04-24 17:34:49 +0900

    初期化時のDBテーブルの構成を変更

--- db/init/001_createhotate.sql
+++ db/init/001_createhotate.sql
@@ -1,10 +1,30 @@
 CREATE DATABASE IF NOT EXISTS hotate;
 
 use hotate;
-CREATE TABLE IF NOT EXISTS books (
-  id int(10) NOT NULL,
-  name VARCHAR(16) NOT NULL,
-  age int(10) NOT NULL,
-  profile VARCHAR(64) NOT NULL,
+
+CREATE TABLE IF NOT EXISTS authors (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
   PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS publishers (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS books (
+  id int NOT NULL AUTO_INCREMENT,
+  isbn int NULL,
+  book_name varchar(255) NOT NULL,
+  book_sub_name varchar(255) NULL,
+  author_id int NOT NULL,
+  ndc int NULL,
+  publisher_id int NOT NULL,
+  year int NOT NULL,
+  book_content text NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (publisher_id) REFERENCES publishers(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
