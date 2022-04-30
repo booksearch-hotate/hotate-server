27a6d192616cece0f500b2b48e23913e9b1fdbba
poyuaki <csubaru07@gmail.com>
2022-04-30 17:19:05 +0900

    デフォルト値を設定

--- db/init/001_createhotate.sql
+++ db/init/001_createhotate.sql
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS publishers (
 
 CREATE TABLE IF NOT EXISTS books (
   id int NOT NULL AUTO_INCREMENT,
-  isbn int NULL,
+  isbn varchar(30) NULL DEFAULT NULL,
   book_name varchar(255) NOT NULL,
-  book_sub_name varchar(255) NULL,
+  book_sub_name varchar(255) NULL DEFAULT NULL,
   author_id int NOT NULL,
-  ndc int NULL,
+  ndc int NULL DEFAULT NULL,
   publisher_id int NOT NULL,
-  year int NULL,
-  book_content text NULL,
+  year int NULL DEFAULT NULL,
+  book_content text NULL DEFAULT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (author_id) REFERENCES authors(id),
   FOREIGN KEY (publisher_id) REFERENCES publishers(id)
