23056f5543d9e4a79a9218556a4a02dc7ccc8203
poyuaki <csubaru07@gmail.com>
2022-05-01 01:21:09 +0900

    インタフェースの実装

--- app/src/db/book.ts
+++ app/src/db/book.ts
@@ -1,6 +1,54 @@
-import { Sequelize, Model, DataTypes } from 'sequelize'
+import { Sequelize, Model, DataTypes, ModelAttributes } from 'sequelize'
+
 import Publisher from './publisher'
 import Author from './author'
+
+import { IRequiredKeys, IOptionalKeys } from '../interfaces/IDbColumn'
+
+const initColumn: IRequiredKeys & IOptionalKeys = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  isbn: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  },
+  book_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  book_sub_name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  },
+  author_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  ndc: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: null,
+  },
+  publisher_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  year: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: null,
+  },
+  book_content: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  }
+}
 
 export default class Book extends Model {
   public id!: number
@@ -14,50 +62,7 @@ export default class Book extends Model {
   public book_content!: string
 
   public static initialize (sequelize: Sequelize) {
-    this.init({
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      isbn: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      },
-      book_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      book_sub_name: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      },
-      author_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      ndc: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: null,
-      },
-      publisher_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      year: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: null,
-      },
-      book_content: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      },
-    }, {
+    this.init(initColumn as unknown as ModelAttributes, {
       sequelize,
       tableName: 'books',
       timestamps: false
--- app/src/interfaces/IDBColumn.ts
+++ app/src/interfaces/IDBColumn.ts
@@ -1,15 +1,26 @@
+import { DataTypes } from 'sequelize'
+
+export interface IAttributeObj {
+  type: DataTypes.DataType,
+  primaryKey?: boolean,
+  autoIncrement?: boolean,
+  allowNull?: boolean,
+  defaultValue?: unknown,
+}
+
 /* 必須となる項目 */
 export interface IRequiredKeys {
-  book_name: string,
-  author_id: number,
-  publisher_id: number,
+  id?: IAttributeObj,
+  book_name: string | IAttributeObj,
+  author_id: number | IAttributeObj,
+  publisher_id: number | IAttributeObj
 }
 
 /* 任意となる項目 */
 export interface IOptionalKeys {
-  isbn?: string,
-  book_sub_name?: string,
-  ndc?: number,
-  year?: number,
-  book_content?: string
+  isbn?: string | IAttributeObj,
+  book_sub_name?: string | IAttributeObj,
+  ndc?: number | IAttributeObj,
+  year?: number | IAttributeObj,
+  book_content?: string | IAttributeObj
 }
