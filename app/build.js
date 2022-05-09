c0d25f8842d0328d60bb71e7c193c31d5cf1100a
poyuaki <csubaru07@gmail.com>
2022-05-09 20:41:55 +0900

    esbuild用のファイルの作成

---
+++ app/build.js
@@ -0,0 +1,22 @@
+const { build } = require('esbuild')
+const glob = require('glob')
+const entryPoints = glob.sync('./src/**/*.ts')
+
+const cmdList = process.argv.slice(2) // コマンドリスト
+
+const isProd = cmdList.includes('pro')
+
+console.log(`This build is ${isProd ? 'production' : 'development'} mode.`)
+
+build({
+  define: { 'process.env.NODE_ENV': process.env.NODE_ENV },
+  entryPoints,
+  outbase: './src',
+  outdir: './dist', // 出力先ディレクトリ
+  platform: 'node',
+  external: [], // バンドルに含めたくないライブラリがある場合は、パッケージ名を文字列で列挙する,
+  watch: false, // trueにすれば、ファイルを監視して自動で再ビルドしてくれるようになる
+  format: 'cjs', // commonjs
+  sourcemap: isProd ? false : 'linked', // 開発環境ならソースマップを出力
+  minify: isProd ? true : false, // 本番環境なら圧縮
+})
