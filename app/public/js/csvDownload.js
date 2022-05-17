e0b9d3cb21f1eaa99b060f0dad8fa75997772282
poyuaki <csubaru07@gmail.com>
2022-05-17 19:15:35 +0900

    ダウンロード時のUIを修正

--- app/public/js/csvDownload.js
+++ app/public/js/csvDownload.js
@@ -2,13 +2,25 @@ const link = 'ws://localhost:5051/'
 
 const ws = new WebSocket(link)
 
+const headerText = document.getElementById('header-text')
+
+const linkBox = document.getElementById('link-box')
+
 ws.onopen = () => {
   console.log('connected')
+  headerText.innerText = '情報を取得中...'
 }
 
 ws.onmessage = (event) => {
   const data = JSON.parse(event.data)
   const progress = data.progress // 現在の進捗状況(string)
+
+  console.log(data)
+
+  if (progress === 'init') {
+    headerText.innerText = 'データを初期化中...'
+    console.log('init')
+  }
 
   const percent = Math.round(data.data.current / data.data.total * 100)
 
@@ -18,14 +30,16 @@ ws.onmessage = (event) => {
   
     progressBar.style.width = percent + '%'
     progressBarText.innerText = percent + '%'
+
+    headerText.innerText = `データを取得中... ${data.data.current} / ${data.data.total}`
   }
 
   if (progress === 'complete') {
-    const completeBox = document.getElementById('complete-box')
-    completeBox.style.display = 'block'
+    linkBox.style.display = 'block'
+    headerText.innerText = 'データの取得が完了しました'
   } else if (progress === 'error') {
-    const errorBox = document.getElementById('error-box')
-    errorBox.style.display = 'block'
+    linkBox.style.display = 'block'
+    headerText.innerText = 'データの取得に失敗しました'
   }
 }
 
