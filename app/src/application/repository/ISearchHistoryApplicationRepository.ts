f5aeb35ad6268d57ff31e92adcac8cf59a35f533
poyuaki <csubaru07@gmail.com>
2022-06-03 21:07:37 +0900

    データ型の修正

--- app/src/application/repository/ISearchHistoryApplicationRepository.ts
+++ app/src/application/repository/ISearchHistoryApplicationRepository.ts
@@ -2,5 +2,6 @@ import SearchHistoryModel from '../../domain/model/searchHistoryModel';
 
 export interface ISearchHistoryApplicationRepository {
   add (searchHistoryModel: SearchHistoryModel): Promise<void>
-  search (words: string): Promise<string[]>
+  search (words: string): Promise<SearchHistoryModel[]>
+  find (count: number): Promise<SearchHistoryModel[]>
 }
