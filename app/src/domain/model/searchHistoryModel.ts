e118bf8c7a8fc36c8cc2e12fb44fe5db5dd39c9d
poyuaki <csubaru07@gmail.com>
2022-06-03 21:06:20 +0900

    searchHistoryのモデルにidを追加

--- app/src/domain/model/searchHistoryModel.ts
+++ app/src/domain/model/searchHistoryModel.ts
@@ -1,13 +1,20 @@
 export default class SearchHistoryModel {
   private words: string;
+  private id: string;
 
-  public constructor(words: string) {
+  public constructor(id:string, words: string) {
     if (words === undefined) throw new Error('wordsが不明です.');
+    if (id === undefined) throw new Error('idが不明です.');
 
     this.words = words;
+    this.id = id;
   }
 
   get Words(): string {
     return this.words;
+  }
+
+  get Id(): string {
+    return this.id;
   }
 }
