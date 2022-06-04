57e8f3f36c70a0f7f252a8f5f64d6e26fab2fc37
poyuaki <csubaru07@gmail.com>
2022-06-04 12:09:06 +0900

    不要な箇所の削除

--- app/src/infrastructure/elasticsearch/esCsv.ts
+++ app/src/infrastructure/elasticsearch/esCsv.ts
@@ -42,27 +42,4 @@ export default class EsCsv extends ElasticSearch {
   private removeBulkApiFile() {
     fs.unlinkSync(this.bulkApiPath);
   }
-
-  public async searchBooks(searchWords: string): Promise<string[]> {
-    const res = await axios.get(`${this.uri}/_search`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      data: {
-        query: {
-          bool: {
-            should: [
-              {match: {'book_name': searchWords}},
-              {match: {'book_content': searchWords}},
-            ],
-          },
-        },
-      },
-    });
-    const hits = res.data.hits.hits;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const ids = hits.map((hit: any) => hit._source.db_id);
-
-    return ids;
-  }
 }
