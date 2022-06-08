46b68f5bbf83cc314e355f9be150974cc61826b2
poyuaki <csubaru07@gmail.com>
2022-06-08 18:54:21 +0900

    検証方法の変更に合わせて修正

--- app/src/application/session/IAdminSession.ts
+++ app/src/application/session/IAdminSession.ts
@@ -2,6 +2,5 @@ import AdminData from '../dto/AdminData';
 
 export interface IAdminSession {
   create (adminData: AdminData): string
-  verify (token: string): boolean
-  get Token (): string
+  verifyToken (token: string): boolean
 }
