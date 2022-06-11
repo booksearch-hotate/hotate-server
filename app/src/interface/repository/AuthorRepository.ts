54bd5a60220413babbd2a0eb97ed57994d8df2f3
poyuaki <csubaru07@gmail.com>
2022-06-11 17:13:47 +0900

    どの本からも参照されていない著者を削除

--- app/src/interface/repository/AuthorRepository.ts
+++ app/src/interface/repository/AuthorRepository.ts
@@ -1,4 +1,6 @@
 import Author from '../../infrastructure/db/tables/author';
+import Book from '../../infrastructure/db/tables/book';
+
 import {IAuthorApplicationRepository} from '../../application/repository/IAuthorApplicationRepository';
 import {IAuthorDomainRepository} from '../../domain/service/repository/IAuthorDomainRepository';
 import AuthorModel from '../../domain/model/authorModel';
@@ -6,9 +8,12 @@ import EsCsv from '../../infrastructure/elasticsearch/esCsv';
 
 import {IEsAuthor} from '../../infrastructure/elasticsearch/IElasticSearchDocument';
 
+import sequelize from 'sequelize';
+
 /* Sequelizeを想定 */
 interface sequelize {
-  Author: typeof Author
+  Author: typeof Author,
+  Book: typeof Book
 }
 
 export default class AuthorRepository implements IAuthorApplicationRepository, IAuthorDomainRepository {
@@ -58,5 +63,35 @@ export default class AuthorRepository implements IAuthorApplicationRepository, I
     });
     if (author) return new AuthorModel(author.id, author.name);
     throw new Error('Author not found');
+  }
+
+  public async deleteNoUsed(authorId: string): Promise<void> {
+    const sql = await this.db.Author.findOne({
+      attributes: [
+        'id',
+        [sequelize.fn('count', sequelize.col('Author.id')), 'count'],
+      ],
+      where: {
+        id: authorId,
+      },
+      group: ['Books.author_id'],
+      include: [{
+        model: this.db.Book,
+        required: true,
+        attributes: [],
+      }],
+    });
+
+    if (sql === null) return;
+
+    const count = sql.getDataValue('count');
+
+    if (Number(count) === 0) {
+      await this.db.Author.destroy({
+        where: {
+          id: authorId,
+        },
+      });
+    }
   }
 }
