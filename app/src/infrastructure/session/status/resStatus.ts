dc4381de4a7374014c89fbda4a74842fc21e997f
poyuaki <csubaru07@gmail.com>
2022-06-14 19:45:52 +0900

    warningを追加

--- app/src/infrastructure/session/status/resStatus.ts
+++ app/src/infrastructure/session/status/resStatus.ts
@@ -1,6 +1,7 @@
-type ResStatus = Success | Failure;
+type ResStatus = Success | Failure | Warning;
 
 type Success = {type: 'Success', mes: string};
+type Warning = {type: 'Warning', mes: string};
 type Failure = {type: 'Failure'; error: Error, mes: string};
 
 export default ResStatus;
