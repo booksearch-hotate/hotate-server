5f959258630aeabbfd2a122f99cc01a0f7a23590
elca-hub <csubaru07@gmail.com>
2022-06-16 19:51:42 +0900

    コマンドラインに関するコメントを追加

--- app/src/infrastructure/cli/cmdLine.ts
+++ app/src/infrastructure/cli/cmdLine.ts
@@ -1,9 +1,19 @@
 const cmdList = process.argv.slice(2);
 
+/**
+ * コマンドラインに`local`が含まれているかを取得します。
+ *
+ * @returns localが含まれているか
+ */
 export function isLocal() {
   return cmdList.includes('local');
 }
 
+/**
+ * コマンドラインに`output-log`が含まれているかを取得します。
+ *
+ * @returns output-logが含まれているか
+ */
 export function isLogOutput() {
   return cmdList.includes('output-log');
 }
