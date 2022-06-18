c73f3e5afd6d87ffec0522f908e469cbb47ee17c
elca-hub <csubaru07@gmail.com>
2022-06-18 15:32:09 +0900

    like検索の場合の全体の検索数を取得するメソッドを削除

--- app/src/application/BookApplicationService.ts
+++ app/src/application/BookApplicationService.ts
@@ -149,8 +149,6 @@ export default class BookApplicationService {
    * @returns 本の総数
    */
   public async getTotalResults(searchWords: string, searchMode: searchMode): Promise<number> {
-    if (searchMode === 'strict') return this.bookRepository.latestEsTotalCount();
-
     if (searchMode === 'tag') return await this.bookRepository.getCountUsingTag(searchWords);
 
     return this.bookRepository.latestEsTotalCount();
