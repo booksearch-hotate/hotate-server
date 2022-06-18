50efdf2b0d7b475a941c92c69c2e3ee6c7680eb8
elca-hub <csubaru07@gmail.com>
2022-06-18 15:27:33 +0900

    elasticsearchでの全文検索の実装

--- app/src/infrastructure/elasticsearch/esBook.ts
+++ app/src/infrastructure/elasticsearch/esBook.ts
@@ -82,6 +82,40 @@ export default class EsSearchBook extends EsCsv {
     });
   }
 
+  public async searchUsingLike(word: string, pageCount: number) {
+    const res = await axios.get(`${this.uri}/_search`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        query: {
+          bool: {
+            must: [{
+              wildcard: {
+                book_name: `*${word}*`,
+              },
+            }],
+          },
+        },
+        from: pageCount * 10,
+        size: 10,
+        sort: {
+          '_score': {
+            order: 'desc',
+          },
+        },
+      },
+    });
+    const hits = res.data.hits.hits;
+
+    this.total = res.data.hits.total.value;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const ids = hits.map((hit: any) => hit._source.db_id);
+
+    return ids;
+  }
+
   get Total(): number {
     return this.total;
   }
