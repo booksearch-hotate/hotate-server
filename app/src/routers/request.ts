2701ad2df9655c6309d0e34954905c65d2fc39a4
elca-hub <csubaru07@gmail.com>
2022-07-04 23:02:11 +0900

    リクエスト内容を確認する処理を追加

--- app/src/routers/request.ts
+++ app/src/routers/request.ts
@@ -45,23 +45,101 @@ requestRouter.get('/request', csrfProtection, async (req: Request, res: Response
   res.render('pages/request', {pageData});
 });
 
+requestRouter.post('/confirm', csrfProtection, async (req: Request, res: Response) => {
+  const bookName = req.body.bookName;
+  const authorName = req.body.authorName;
+  const publisherName = req.body.publisherName;
+  const isbn = req.body.isbn;
+  const message = req.body.message;
+
+  const departmentId = req.body.department;
+  const schoolYear = req.body.schoolYear;
+  const schoolClass = req.body.class;
+  const userName = req.body.userName;
+
+  const keepReqObj = {
+    bookName,
+    authorName,
+    publisherName,
+    isbn,
+    message,
+    departmentId,
+    schoolYear,
+    schoolClass,
+    userName,
+  };
+
+  req.session.keepValue = {keepReqObj};
+  return res.redirect('/confirm-request');
+});
+
+requestRouter.get('/confirm-request', csrfProtection, async (req: Request, res: Response) => {
+  pageData.headTitle = 'リクエスト内容の確認 | HOTATE';
+
+  pageData.csrfToken = req.csrfToken();
+
+  try {
+    if (typeof req.session.keepValue !== 'object') throw new Error('The value could not be obtained correctly.');
+
+    const keepReqObj = req.session.keepValue.keepReqObj;
+
+    const reqData = requestApplicationService.makeData(
+        keepReqObj.bookName,
+        keepReqObj.authorName,
+        keepReqObj.publisherName,
+        keepReqObj.isbn,
+        keepReqObj.message,
+        keepReqObj.departmentId,
+        keepReqObj.schoolYear,
+        keepReqObj.schoolClass,
+        keepReqObj.userName,
+    );
+
+    pageData.anyData = {request: reqData};
+
+    return res.render('pages/confirm-request', {pageData});
+  } catch (e: any) {
+    logger.error(e);
+    return res.redirect('/');
+  }
+});
+
 requestRouter.post('/register', csrfProtection, async (req: Request, res: Response) => {
   try {
-    const bookName = req.body.bookName;
-    const authorName = req.body.authorName;
-    const publisehrName = req.body.publisehrName;
-    const isbn = req.body.isbn;
-    const message = req.body.message;
+    /* 上の行の処理とも同じなので共通化した方が良い */
+    if (typeof req.session.keepValue !== 'object') throw new Error('The value could not be obtained correctly.');
 
-    const departmentId = req.body.department;
-    const schoolYear = req.body.schoolYear;
-    const schoolClass = req.body.class;
-    const userName = req.body.userName;
+    const keepReqObj = req.session.keepValue.keepReqObj;
+
+    const requestData = requestApplicationService.makeData(
+        keepReqObj.bookName,
+        keepReqObj.authorName,
+        keepReqObj.publisherName,
+        keepReqObj.isbn,
+        keepReqObj.message,
+        keepReqObj.departmentId,
+        keepReqObj.schoolYear,
+        keepReqObj.schoolClass,
+        keepReqObj.userName,
+    );
+
+    const id = requestData.Id;
+    const bookName = requestData.BookName;
+    const authorName = requestData.AuthorName;
+    const publisherName = requestData.PublisherName;
+    const isbn = requestData.Isbn;
+    const message = requestData.Message;
+
+    const departmentId = requestData.DepartmentId;
+    const schoolYear = requestData.SchoolYear;
+    const schoolClass = requestData.SchoolClass;
+    const userName = requestData.UserName;
 
     await requestApplicationService.register(
+        id,
         bookName,
         authorName,
-        publisehrName,
+        publisherName,
         isbn,
         message,
         departmentId,
@@ -70,7 +148,10 @@ requestRouter.post('/register', csrfProtection, async (req: Request, res: Respon
         userName,
     );
     logger.info(`Request received. Sender Name: ${userName}`);
+
     req.session.status = {type: 'Success', mes: 'リクエストの記録が完了しました！リクエストありがとうございます！'};
+    req.session.keepValue = undefined;
+
     res.redirect('/');
   } catch (e: any) {
     logger.error(e);
