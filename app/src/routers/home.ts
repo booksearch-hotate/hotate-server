9318666dd6602cc5bc37f64f9a937573b625cdfc
Elca <csubaru07@gmail.com>
2022-07-18 23:22:16 +0900

    ホーム画面におすすめセクションの一覧を表示

--- app/src/routers/home.ts
+++ app/src/routers/home.ts
@@ -1,16 +1,54 @@
 import {Request, Response, Router} from 'express';
 
+import RecommendationApplicationService from '../application/recommendationApplicationService';
+import RecommendationService from '../domain/service/recommendationService';
+import db from '../infrastructure/db';
+import RecommendationRepository from '../interface/repository/recommendationRepository';
+
+import Logger from '../infrastructure/logger/logger';
+
 import {IPage} from './datas/IPage';
+import BookApplicationService from '../application/bookApplicationService';
+import BookRepository from '../interface/repository/bookRepository';
+import EsSearchBook from '../infrastructure/elasticsearch/esBook';
+import BookService from '../domain/service/bookService';
 
 // eslint-disable-next-line new-cap
 const homeRouter = Router();
 
 const pageData: IPage = {} as IPage;
 
-homeRouter.get('/', (req: Request, res: Response) => {
-  pageData.headTitle = 'ホーム | HOTATE';
+const logger = new Logger('home');
 
-  res.render('pages/index', {pageData});
+const recommendationApplicationService = new RecommendationApplicationService(
+    new RecommendationRepository(db),
+    new RecommendationService(),
+);
+
+const bookApplicationService = new BookApplicationService(
+    new BookRepository(db, new EsSearchBook('books')),
+    new BookService(),
+);
+
+homeRouter.get('/', async (req: Request, res: Response) => {
+  try {
+    const fetchDatas = recommendationApplicationService.omitContent(await recommendationApplicationService.fetch(0, 5));
+
+    const recommendations = await Promise.all(fetchDatas.map(async (recommendation) => {
+      const books = await Promise.all(recommendation.BookIds.map(async (bookId) => await bookApplicationService.searchBookById(bookId)));
+      return {recommendation, books};
+    }));
+
+    pageData.anyData = {
+      recommendations,
+    };
+  } catch (e: any) {
+    logger.error(e);
+  } finally {
+    pageData.headTitle = 'ホーム | HOTATE';
+
+    res.render('pages/index', {pageData});
+  }
 });
 
 export default homeRouter;
