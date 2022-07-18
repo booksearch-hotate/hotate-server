20f174aa96f68b5d5c1e56e432e821e23d2fcfcf
Elca <csubaru07@gmail.com>
2022-07-18 23:38:25 +0900

    本の個数が超過しているかの振る舞いを追加

--- app/src/domain/service/recommendationService.ts
+++ app/src/domain/service/recommendationService.ts
@@ -1,7 +1,14 @@
 import {v4 as uuidv4} from 'uuid';
+import RecommendationModel from '../model/recommendation/recommendationModel';
 
 export default class RecommendationService {
+  private readonly MAX_HAVING_BOOK_COUNT = 2;
+
   public createUUID(): string {
     return uuidv4();
+  }
+
+  public isOverNumberOfBooks(recommendation: RecommendationModel) {
+    return recommendation.BookIds.length > this.MAX_HAVING_BOOK_COUNT;
   }
 }
