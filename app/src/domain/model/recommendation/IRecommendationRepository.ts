0eecae2bdff5a2a73021b5a3d6b28ca4cd8dd1d8
Elca <csubaru07@gmail.com>
2022-07-18 23:21:57 +0900

    個数の指定

--- app/src/domain/model/recommendation/IRecommendationRepository.ts
+++ app/src/domain/model/recommendation/IRecommendationRepository.ts
@@ -2,7 +2,7 @@ import RecommendationModel from './recommendationModel';
 export interface IRecommendationRepository {
   insert(recommendationModel: RecommendationModel): Promise<void>
   findMaxIndex(): Promise<number>
-  fetch(pageCount: number): Promise<RecommendationModel[]>
+  fetch(pageCount: number, count: number): Promise<RecommendationModel[]>
   fetchAllCount(): Promise<number>
   findById(id: string): Promise<RecommendationModel | null>
   update(recommendation: RecommendationModel): Promise<void>
