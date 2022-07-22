a629f62fd05d3395f692fbab4df7cc215a9043b8
Elca <csubaru07@gmail.com>
2022-07-22 23:18:11 +0900

    idが文字列以外の場合エラーをthrowするように

--- app/src/domain/model/book/bookIdModel.ts
+++ app/src/domain/model/book/bookIdModel.ts
@@ -2,7 +2,7 @@ export default class bookIdModel {
   private id: string;
 
   constructor(id: string) {
-    if (id.length === 0 || id.length > 36) throw new Error('Invalid book id.');
+    if (typeof id !== 'string' || id.length === 0 || id.length > 36) throw new Error('Invalid book id.');
 
     this.id = id;
   }
