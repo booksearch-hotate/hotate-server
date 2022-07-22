7e82f494bde1370cee8c364b3abcad056e94994d
Elca <csubaru07@gmail.com>
2022-07-23 00:01:16 +0900

    本とコメントの固まりをモデル化

---
+++ app/src/domain/model/recommendation/recommendationItemModel.ts
@@ -0,0 +1,23 @@
+import BookIdModel from '../book/bookIdModel';
+
+export default class RecommendationItemModel {
+  private bookId: BookIdModel;
+  private comment: string | null;
+
+  private readonly MAX_COMMENT_LEN = 100;
+
+  constructor(bookId: BookIdModel, comment: string | null) {
+    if (comment !== null && comment.length > this.MAX_COMMENT_LEN) throw new Error('Have exceeded the number of characters you can enter.');
+
+    this.bookId = bookId;
+    this.comment = comment;
+  }
+
+  get Comment() {
+    return this.comment;
+  }
+
+  get BookId() {
+    return this.bookId;
+  }
+}
