e3ed7b1b80ebfa3d3bf3c401d41c5fe04e88616b
elca-hub <csubaru07@gmail.com>
2022-09-10 14:32:52 +0900

    管理者画面に関するテストケースを作成

--- app/__tests__/e2e/server.test.ts
+++ app/__tests__/e2e/server.test.ts
@@ -3,9 +3,51 @@ import {app} from '../../src/handler/app';
 
 const request = supertest(app);
 
+const fetchCsrfData = async (inputPath: string): Promise<{token: string, cookie: string}> => {
+  const loginPage = await request.get(inputPath);
+
+  const dom = loginPage.text;
+
+  const csrtTokenMetaTag = '<meta name="csrfToken" content="';
+
+  const startIndex = dom.indexOf(csrtTokenMetaTag) + csrtTokenMetaTag.length;
+
+  const endIndex = dom.substring(startIndex).indexOf('">') + startIndex;
+
+  return {
+    token: dom.substring(startIndex, endIndex),
+    cookie: loginPage.headers['set-cookie'],
+  };
+};
+
 describe('Test the root path', () => {
   test('It should response the GET method', async () => {
     const res = await request.get('/');
     expect(res.statusCode).toBe(200);
+  });
+});
+
+describe('Request admin page', () => {
+  test('Access directly', async () => {
+    const res = await request.get('/admin/');
+    expect(res.statusCode).toBe(302);
+  });
+
+  test('Access with right credentials', async () => {
+    const csrfTokenData = await fetchCsrfData('/login');
+
+    const userData = {
+      id: 'akamurasaki',
+      pw: 'aiueo',
+      _csrf: csrfTokenData.token,
+    };
+
+    const res = await request
+        .post('/check')
+        .type('form')
+        .set('Cookie', csrfTokenData.cookie)
+        .send(userData);
+
+    expect(res.statusCode).toBe(302);
   });
 });
