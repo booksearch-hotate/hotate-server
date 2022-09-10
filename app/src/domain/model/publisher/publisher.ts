ff3c4d04e4cb026ce905751c31ba4295b2073e22
elca-hub <csubaru07@gmail.com>
2022-09-10 23:20:14 +0900

    出版社モデルに文字列の制限を追加

--- app/src/domain/model/publisher/publisher.ts
+++ app/src/domain/model/publisher/publisher.ts
@@ -1,9 +1,14 @@
+import {DomainInvalidError} from '../../../presentation/error';
+
 export default class Publisher {
   private id: string;
   private name!: string | null;
 
+  private readonly MAX_NAME_LEN = 200;
+
   public constructor(id: string, name: string | null) {
-    if (id === null) throw new Error('idがnullです');
+    if (id === null) throw new DomainInvalidError('id is null.');
+    if (name !== null && name.length > this.MAX_NAME_LEN) throw new DomainInvalidError(`Over length of autor name. Length of author name: ${name.length}`);
 
     this.id = id;
     this.name = name;
