5ad8ef499c8deeb72d7e61d4400ad9bf1b10b0ba
elca-hub <csubaru07@gmail.com>
2022-09-10 16:18:53 +0900

    管理者モデルに文字数の制限を追加

--- app/src/domain/model/admin/admin.ts
+++ app/src/domain/model/admin/admin.ts
@@ -1,10 +1,15 @@
+import {DomainInvalidError} from '../../../presentation/error';
+
 export default class Admin {
   private id: string;
   private pw: string;
 
+  private readonly MAX_ID_LEN = 30;
+  private readonly MAX_PW_LEN = 60;
+
   public constructor(id: string | undefined, pw: string | undefined) {
-    if (id === undefined || id.length === 0) throw new Error('The id is unknown.');
-    if (pw === undefined || pw.length === 0) throw new Error('The password is unknown.');
+    if (id === undefined || id.length === 0 || id.length > this.MAX_ID_LEN) throw new DomainInvalidError(`The format of the id is different. id: ${id}`);
+    if (pw === undefined || pw.length === 0 || pw.length > this.MAX_PW_LEN) throw new DomainInvalidError(`The format of the password is different. password: ${pw}`);
 
     this.id = id;
     this.pw = pw;
