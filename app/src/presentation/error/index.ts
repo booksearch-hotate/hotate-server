3a09e072ef480fd60fef115fe211860af8b7f263
elca-hub <csubaru07@gmail.com>
2022-09-16 15:21:10 +0900

    エラークラスに対するコメントアウトを追加

--- app/src/presentation/error/index.ts
+++ app/src/presentation/error/index.ts
@@ -1,9 +1,40 @@
+/**
+ * Formの値が正しく入力されていない場合にthrowされるべきエラークラス
+ *
+ * @export
+ * @class FormInvalidError
+ * @extends {Error}
+ */
 export class FormInvalidError extends Error {};
 
+/**
+ * ドメインモデル内でルールに反する処理が行われたときにthrowされるべきエラークラス
+ *
+ * @export
+ * @class DomainInvalidError
+ * @extends {Error}
+ */
 export class DomainInvalidError extends Error {};
 
+/**
+ * インメモリで動作しているDBに関するエラーの際にthrowされるべきエラークラス
+ *
+ * @export
+ * @class InMemoryDBError
+ * @extends {Error}
+ */
 export class InMemoryDBError extends Error {};
 
+/**
+ * MySQLに関するエラーの際にthrowされるべきエラークラス
+ */
 export class MySQLDBError extends Error {};
 
+/**
+ * インフラに関するエラーの際にthrowされるべきエラークラス
+ *
+ * @export
+ * @class InfrastructureError
+ * @extends {Error}
+ */
 export class InfrastructureError extends Error {};
