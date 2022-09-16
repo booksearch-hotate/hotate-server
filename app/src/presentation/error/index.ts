2e1a4c366c239ff5acbd093f9a9c04a3ab4e202e
elca-hub <csubaru07@gmail.com>
2022-09-16 12:45:36 +0900

    MySQLに関するDBのエラーを実装したクラスを追加

--- app/src/presentation/error/index.ts
+++ app/src/presentation/error/index.ts
@@ -3,3 +3,6 @@ export class FormInvalidError extends Error {};
 export class DomainInvalidError extends Error {};
 
 export class InMemoryDBError extends Error {};
+
+export class MySQLDBError extends Error {};
+
