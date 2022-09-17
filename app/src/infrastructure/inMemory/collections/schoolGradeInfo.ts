205e23c36a0d0e2b9114c1b2eb613efd5d6d2b05
elca-hub <csubaru07@gmail.com>
2022-09-17 13:25:58 +0900

    学校情報を格納するコレクションの追加

---
+++ app/src/infrastructure/inMemory/collections/schoolGradeInfo.ts
@@ -0,0 +1,9 @@
+import {ObjectId} from 'mongodb';
+
+export type schoolGradeInfoDocument = {
+  _id?: ObjectId
+  year: number;
+  school_class: number;
+}
+
+export const SchoolGradeInfoCollectionName = 'schoolGradeInfos';
