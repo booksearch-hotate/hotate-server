204913b15bd4d790499019b00121f51ec1aaa415
elca-hub <csubaru07@gmail.com>
2022-09-17 14:12:08 +0900

    タイムゾーンをAsia/Tokyoに統一

--- app/src/utils/conversionDate.ts
+++ app/src/utils/conversionDate.ts
@@ -1,3 +1,10 @@
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(timezone);
+dayjs.extend(utc);
+
 /**
  * Date型を`YYYY年MM月DD日 hh時mm分ss秒`の文字列型に変換します。なお自動的に0埋めも行います。
  *
@@ -6,22 +13,6 @@
  * @return {*}  {string} 変換後の文字列
  */
 export default function conversionDate(date: Date): string {
-  const year = date.getFullYear();
-  // 月だけ+1すること
-  const month = 1 + date.getMonth();
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
-  const second = date.getSeconds();
-
-  const yearStr = String(year);
-  const monthStr = (`0${month}`).slice(-2);
-  const dayStr = (`0${day}`).slice(-2);
-  const hourStr = (`0${hour}`).slice(-2);
-  const minuteStr = (`0${minute}`).slice(-2);
-  const secondStr = (`0${second}`).slice(-2);
-
-  const res = `${yearStr}年${monthStr}月${dayStr}日 ${hourStr}時${minuteStr}分${secondStr}秒`;
-
+  const res = dayjs(date).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');
   return res;
 }
