ac9d854c0695991b5185541df41906a0380b71d7
elca-hub <csubaru07@gmail.com>
2022-09-19 16:40:58 +0900

    画像を指定されたサイズに切り取る関数を作成

---
+++ app/src/utils/conversionImgSize.ts
@@ -0,0 +1,13 @@
+import sharp from 'sharp';
+import smartcrop from 'smartcrop-sharp';
+import fs from 'fs';
+
+export default async function conversionImgSize(input: string, output: string, width=1280, height=720) {
+  const body = fs.readFileSync(input);
+  const result = await smartcrop.crop(body, {width: width, height: height});
+  const crop = result.topCrop;
+  return await sharp(body)
+      .extract({width: crop.width, height: crop.height, left: crop.x, top: crop.y})
+      .resize(width, height)
+      .toFile(output);
+};
