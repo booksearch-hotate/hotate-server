c8299faf6574b6297919d84769638490162e853b
elca-hub <csubaru07@gmail.com>
2022-09-26 21:28:16 +0900

    output-logがありかつlocalがある場合、全レベルの表示を行う

--- app/src/infrastructure/logger/logger.ts
+++ app/src/infrastructure/logger/logger.ts
@@ -1,11 +1,11 @@
 import log4js from 'log4js';
-import {isLogOutput} from '../cli/cmdLine';
+import {isLocal, isLogOutput} from '../cli/cmdLine';
 
 export default class Logger {
   private logger: log4js.Logger; // コンソールへの出力
 
   constructor(name: string) {
-    const defaultCategories = isLogOutput() ? {appenders: ['out', 'system'], level: 'info'} : {appenders: ['system'], level: 'debug'};
+    const defaultCategories = isLogOutput() ? {appenders: ['system', 'wrapErr'], level: isLocal() ? 'all' : 'info'} : {appenders: ['system'], level: 'debug'};
     log4js.configure({
       appenders: {
         system: {type: 'stdout'}, // 標準出力
