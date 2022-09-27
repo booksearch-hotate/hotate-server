fb82b29b2b416f90bda77dfd6d0085670344f720
elca-hub <csubaru07@gmail.com>
2022-09-27 21:24:57 +0900

    名称の変更

--- app/src/utils/setPageData.ts
+++ app/src/utils/setPageData.ts
@@ -1,6 +1,6 @@
 import {NextFunction, Request, Response} from 'express';
 
-export default function GlobalData(req: Request, res: Response, next: NextFunction) {
+export default function SetPageData(req: Request, res: Response, next: NextFunction) {
   res.pageData = {
     headTitle: '',
     path: req.path,
