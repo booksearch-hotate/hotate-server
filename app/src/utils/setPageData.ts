52d9afcb5154d3c820502bd5e6b6887a624d82ab
elca-hub <csubaru07@gmail.com>
2022-09-30 12:33:02 +0900

    コメントの追加

--- app/src/utils/setPageData.ts
+++ app/src/utils/setPageData.ts
@@ -2,10 +2,10 @@ import {NextFunction, Request, Response} from 'express';
 
 export default function SetPageData(req: Request, res: Response, next: NextFunction) {
   res.pageData = {
-    headTitle: '',
-    path: req.path,
-    csrfToken: '',
-    serviceName: 'TREE',
+    headTitle: '', // head内のtitleタグに記述するメッセージ
+    path: req.path, // パス名
+    csrfToken: '', // csrfトークン
+    serviceName: 'TREE', // サービス名
   };
   next();
 };
