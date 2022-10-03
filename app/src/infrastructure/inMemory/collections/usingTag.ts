d45bd3e63708ad91002677da181310de5ed06cc7
elca-hub <csubaru07@gmail.com>
2022-10-03 19:19:04 +0900

    タグの中間テーブルに当たるコレクションの作成

---
+++ app/src/infrastructure/inMemory/collections/usingTag.ts
@@ -0,0 +1,9 @@
+import {ObjectId} from 'mongodb';
+
+export type usingTagDocument = {
+  _id?: ObjectId;
+  book_id: string;
+  tag_id: string;
+}
+
+export const usingTagCollectionName = 'usingTagss';
