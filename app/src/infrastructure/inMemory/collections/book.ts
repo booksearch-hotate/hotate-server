71190a5c6395625e488ef123eff02e80378d3c90
elca-hub <csubaru07@gmail.com>
2022-10-03 17:51:05 +0900

    nullを許容するように変更

--- app/src/infrastructure/inMemory/collections/book.ts
+++ app/src/infrastructure/inMemory/collections/book.ts
@@ -3,14 +3,14 @@ import {ObjectId} from 'mongodb';
 export type bookDocument = {
   _id?: ObjectId
   id: string;
-  isbn: string;
+  isbn: string | null;
   book_name: string;
-  book_sub_name: string;
+  book_sub_name: string | null;
   author_id: string;
-  ndc: number;
+  ndc: number | null;
   publisher_id: string;
-  year: number;
-  book_content: string;
+  year: number | null;
+  book_content: string | null;
 }
 
 export const bookCollectionName = 'books';
