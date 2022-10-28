c6ec657c1669aa2712366b86ffbd69376ca7f481
elca-hub <csubaru07@gmail.com>
2022-10-28 17:28:23 +0900

    aboutのimgを登録

--- app/public/js/about.js
+++ app/public/js/about.js
@@ -3,3 +3,22 @@ const headerBox = document.getElementById('tree-header');
 const mainAboutBox = document.getElementById('main-about-box');
 
 mainAboutBox.style.height = `${window.innerHeight - headerBox.clientHeight}px`;
+
+const bgImgBox = document.getElementById('bg-img-box');
+
+const bgImgChildren = Array.from(bgImgBox.children);
+
+
+let counter = 0;
+
+function changeImg() {
+  const beforeCounter = counter - 1 < 0 ? bgImgChildren.length - 1 : counter - 1;
+  bgImgChildren[beforeCounter].style.opacity = 0;
+  bgImgChildren[counter].style.opacity = 1;
+  counter = counter + 1 >= bgImgChildren.length ? 0 : counter + 1;
+}
+
+changeImg();
+
+/* aboutのメイン箇所のimg */
+setInterval(()=> {changeImg()}, 10 * 1000);
