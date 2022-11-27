a06724e8789be4e3dd70348c5bbe73d9cc65ebb8
elca-hub <csubaru07@gmail.com>
2022-11-27 18:16:02 +0900

    help画面の作成

---
+++ app/src/routers/admin/help.ts
@@ -0,0 +1,13 @@
+import {Request, Response, Router} from 'express';
+
+// eslint-disable-next-line new-cap
+const helpRouter = Router();
+
+/* ヘルプ画面 */
+helpRouter.get('/', async (req: Request, res: Response) => {
+  res.pageData.headTitle = 'ヘルプ';
+
+  res.render('pages/admin/help/index', {pageData: res.pageData});
+});
+
+export default helpRouter;
