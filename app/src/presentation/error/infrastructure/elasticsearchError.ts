b76241bbc3eefceb21c91012086c2867c836d861
elca-hub <csubaru07@gmail.com>
2023-01-13 13:35:57 +0900

    bulk apiの実行時等にthrowされるエラークラスの作成

--- app/src/presentation/error/infrastructure/elasticsearchError.ts
+++ app/src/presentation/error/infrastructure/elasticsearchError.ts
@@ -3,3 +3,8 @@
   * elasticsearchに関するエラーの際にthrowされるべきエラークラス
   */
 export class ElasticsearchError extends Error {};
+
+/**
+ * esに関するbulk apiのエラーの際にthrowされるべきクラス
+ */
+export class EsBulkApiError extends ElasticsearchError {};
